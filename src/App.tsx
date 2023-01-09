61500a95ac1afbe9d3dec7c7c121f61a5297a8c1
Nishanth Jayram <nishanthjayram@gmail.com>
2023-01-09 14:02:32 -0800

    Modified to initialize and render Canvas component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Canvas from "./components/Canvas/Canvas";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = (props: { canvas_size: number }) => {
+  const { canvas_size } = props;
+  const startCanvasState = new Array(canvas_size ** 2).fill(false); // Initial canvas state (empty)
+
+  const [canvasState, setCanvasState] = useState(startCanvasState);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <h1>Karektar</h1>
+      <div className="App">
+        <Canvas canvasState={canvasState} setCanvasState={setCanvasState} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
