2c4675f04bd512ec7c4a9c8d2fa581be12da5b55
Nishanth Jayram <nishanthjayram@gmail.com>
2023-01-10 21:18:36 -0800

    Changed canvas size variable to camel case.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Canvas from "./components/Canvas/Canvas";
 import { useState } from "react";
 import "./App.css";
 
-const App = (props: { canvas_size: number }) => {
-  const { canvas_size } = props;
-  const startCanvas = new Array(canvas_size ** 2).fill(false); // Initial canvas state (empty)
+const App = (props: { canvasSize: number }) => {
+  const { canvasSize } = props;
+  const startCanvas = new Array(canvasSize ** 2).fill(false); // Initial canvas state (empty)
 
   const [canvas, setCanvas] = useState(startCanvas);
 
