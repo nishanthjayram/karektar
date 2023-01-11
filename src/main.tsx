013b29df4d3f1456b4ca8304841bfb5e4a8315c3
Nishanth Jayram <nishanthjayram@gmail.com>
2023-01-10 21:15:18 -0800

    Updated canvas size parameter for App component.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App canvas_size={8} />
+    <App canvasSize={8} />
   </React.StrictMode>
 );
