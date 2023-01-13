634d042de091d879db662544d4ce7dcd5d056f7f
Nishanth Jayram <nishanthjayram@gmail.com>
2023-01-13 12:22:04 -0800

    Refactored styling into CSS module; added classnames for conditional styling of cells.

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,20 +1,16 @@
-const FILLED_COLOR = "black";
-const EMPTY_COLOR = "white";
+import styles from "./Cell.module.scss";
+import classnames from "classnames";
 
 const Cell = (props: { filled: boolean; toggleCell: () => void }) => {
   const { filled, toggleCell } = props;
 
   return (
     <div
-      style={{
-        width: "50px",
-        height: "50px",
-        backgroundColor: filled ? FILLED_COLOR : EMPTY_COLOR,
-        borderColor: "black",
-        borderWidth: "1px",
-        borderStyle: "solid",
-        boxSizing: "border-box",
-      }}
+      className={classnames(
+        !filled && styles.empty,
+        filled && styles.filled,
+        styles.cell
+      )}
       onClick={toggleCell}
     />
   );
