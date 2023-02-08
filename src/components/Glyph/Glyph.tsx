10e64aa01f12cc4629d135ddbc75af9b5376a981
Nishanth Jayram <nishanthjayram@gmail.com>
2023-02-08 12:38:36 -0800

    Modified state management and props.

--- src/components/Glyph/Glyph.tsx
+++ src/components/Glyph/Glyph.tsx
@@ -7,24 +7,16 @@ export type TGlyphState = boolean[];
 const Glyph = ({
   glyph,
   glyphCanvas,
-  setCanvas,
   setActiveGlyph,
 }: {
   glyph: string;
   glyphCanvas: boolean[];
-  setCanvas: Dispatch<SetStateAction<boolean[]>>;
   setActiveGlyph: Dispatch<SetStateAction<string>>;
 }) => {
   return (
     <div className={styles.glyph}>
       <div className={styles.symbol}>{glyph}</div>
-      <div
-        className={styles.canvas}
-        onClick={() => {
-          setCanvas(glyphCanvas);
-          setActiveGlyph(glyph);
-        }}
-      >
+      <div className={styles.canvas} onClick={() => setActiveGlyph(glyph)}>
         {glyphCanvas.map((isFilled, index) => (
           <GlyphCell key={index} filled={isFilled} />
         ))}
