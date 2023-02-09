af20dad8e8ba7fcf157c3296d413b75aaa32e1c0
Nishanth Jayram <nishanthjayram@gmail.com>
2023-02-08 18:30:05 -0800

    Added memoization of glyph states for performance improvement.

--- src/components/Glyph/Glyph.tsx
+++ src/components/Glyph/Glyph.tsx
@@ -1,28 +1,38 @@
+import { memo } from "react";
 import { Dispatch, SetStateAction } from "react";
 import GlyphCell from "../GlyphCell/GlyphCell";
+import classnames from "classnames";
 import styles from "./Glyph.module.scss";
 
 export type TGlyphState = boolean[];
 
 const Glyph = ({
+  canvasSize,
   glyph,
   glyphCanvas,
+  activeGlyph,
   setActiveGlyph,
 }: {
+  canvasSize: number;
   glyph: string;
   glyphCanvas: boolean[];
-  setActiveGlyph: Dispatch<SetStateAction<string>>;
+  activeGlyph: string | undefined;
+  setActiveGlyph: Dispatch<SetStateAction<string | undefined>>;
 }) => {
   return (
     <div className={styles.glyph}>
       <div className={styles.symbol}>{glyph}</div>
       <div className={styles.canvas} onClick={() => setActiveGlyph(glyph)}>
         {glyphCanvas.map((isFilled, index) => (
-          <GlyphCell key={index} filled={isFilled} />
+          <GlyphCell key={index} canvasSize={canvasSize} filled={isFilled} />
         ))}
       </div>
     </div>
   );
 };
 
-export default Glyph;
+export default memo(Glyph, (prevProps, nextProps) =>
+  prevProps.glyphCanvas.every(
+    (value, index) => value === nextProps.glyphCanvas[index]
+  )
+);
