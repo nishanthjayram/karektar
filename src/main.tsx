7ce5a06492aee7081ccc89042719d7763a7eb868
Nishanth Jayram <nishanthjayram@gmail.com>
2023-02-08 18:27:02 -0800

    Changed default canvas size.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App canvasSize={8} />
+    <App canvasSize={16} />
   </React.StrictMode>
 );
