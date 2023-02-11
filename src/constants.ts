03011fb841c84b4fc5c21f01822ec31dd51b0dc2
Nishanth Jayram <nishanthjayram@gmail.com>
2023-02-10 20:01:30 -0800

    Moved constants to global file.

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+// Regular expressions
+export const RX_LETTERS = /[A-Za-z]/g;
+export const RX_NUMBERS = /[0-9]/g;
+export const RX_NON_ALPHANUMERIC = /[^A-Za-z0-9 ]/g;
+
+// App
+export const DEFAULT_PROMPT = "sphinx of black quartz judge my vow";
+
+// GlyphSet
+export const PAGE_LENGTH = 30; // Number of glyphs to display in a page
