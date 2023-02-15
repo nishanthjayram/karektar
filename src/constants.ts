aa801670498c78d8c3f1ea7d4e7dc80d85fc6254
Nishanth Jayram <nishanthjayram@gmail.com>
2023-02-15 15:17:34 -0800

    Updated regex patterns to exclude any whitespace characters as glyphs.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // Regular expressions
 export const RX_LETTERS = /[A-Za-z]/g
 export const RX_NUMBERS = /[0-9]/g
-export const RX_NON_ALPHANUMERIC = /[^A-Za-z0-9 ]/g
+export const RX_NON_ALPHANUMERIC = /[^A-Za-z0-9\s]/g
 
 // App
 export const DEFAULT_PROMPT = 'sphinx of black quartz judge my vow'
