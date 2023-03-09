761d35dcf320789b84c76871cdcca6251fc6785f
Nishanth Jayram <nishanthjayram@gmail.com>
2023-03-08 18:05:23 -0800

    Added constants for main canvas size and cell states.

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,10 @@ export const RX_NON_ALPHANUMERIC = /[^A-Za-z0-9\s]/g
 // App
 export const DEFAULT_PROMPT = 'sphinx of black quartz judge my vow'
 
+// Canvas
+export const CANVAS_SIZE = 400
+export const EMPTY_CELL = '#efefef'
+export const FILLED_CELL = '#2b2b2b'
+
 // GlyphSet
 export const PAGE_LENGTH = 30 // Number of glyphs to display in a page
