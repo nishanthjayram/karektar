2e5b4b035553ea25b55bb916afedb4498375db6a
Nishanth Jayram <nishanthjayram@gmail.com>
2023-03-08 18:04:42 -0800

    Refactored to use Canvas API without cell components.

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,8 +1,8 @@
 // A canvas for drawing individual glyphs.
-import {Dispatch, SetStateAction, useState} from 'react'
+import {Dispatch, SetStateAction, useEffect, useRef, useState} from 'react'
 import styles from './Canvas.module.scss'
 import {ReactComponent as Delete} from '../../assets/delete.svg'
-import Cell from '../Cell/Cell'
+import {CANVAS_SIZE, EMPTY_CELL, FILLED_CELL} from '../../constants'
 
 const Canvas = ({
   canvasSize,
@@ -15,67 +15,109 @@ const Canvas = ({
   setGlyphSet: Dispatch<SetStateAction<Map<string, boolean[]>>>
   activeGlyph: string | undefined
 }) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const glyphCanvas = activeGlyph ? glyphSet.get(activeGlyph) : undefined
+  const p = CANVAS_SIZE / canvasSize
+
   const [mouseDownFlag, setMouseDownFlag] = useState(false)
   const [drawFlag, setDrawFlag] = useState(true)
 
-  const canvas = activeGlyph ? glyphSet.get(activeGlyph) : undefined
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d')
+    if (!glyphCanvas || !ctx) {
+      return
+    }
+    ctx.strokeStyle = '#ffffff'
+    ctx.beginPath()
+    glyphCanvas.forEach((filled: boolean, idx: number) => {
+      const [x, y] = [idx % canvasSize, Math.floor(idx / canvasSize)]
+      ctx.fillStyle = filled ? FILLED_CELL : EMPTY_CELL
+      ctx.fillRect(x * p + 1, y * p + 1, p - 1, p - 1)
+      ctx.strokeRect(x * p + 1, y * p + 1, p - 1, p - 1)
+    })
+    ctx.closePath()
+  }, [canvasSize, glyphCanvas, p])
 
-  if (canvas) {
+  const getMousePos = (
+    canvas: HTMLCanvasElement,
+    evt: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+  ) => {
+    const rect = canvas.getBoundingClientRect()
+    const x = Math.floor((evt.clientX - rect.left) / p)
+    const y = Math.floor((evt.clientY - rect.top) / p)
     return (
-      <div>
-        <div className={styles.header}>
-          <div className={styles.text}>{activeGlyph}</div>
-          <div className={styles.separator} />
-          <Delete
-            className={styles.deleteIcon}
-            onClick={() =>
-              setGlyphSet(oldGlyphSet => {
-                const newGlyphSet = new Map(oldGlyphSet)
-                const newCanvas = new Array<boolean>(canvasSize ** 2).fill(false)
-                if (activeGlyph) {
-                  newGlyphSet.set(activeGlyph, newCanvas)
-                }
-                return newGlyphSet
-              })
-            }
-          />
-        </div>
-        <div
-          className={styles.canvas}
-          onMouseOver={e => {
-            if (e.buttons === 1) {
-              setMouseDownFlag(true)
-            }
-          }}
-          onMouseLeave={() => setMouseDownFlag(false)}
-        >
-          {canvas.map((isFilled, index) => (
-            <Cell
-              key={index}
-              canvasSize={canvasSize}
-              filled={isFilled}
-              toggleCell={() => {
-                setGlyphSet(oldGlyphSet => {
-                  const newGlyphSet = new Map(oldGlyphSet)
-                  const newCanvas = [...canvas]
-                  newCanvas[index] = !canvas[index]
-                  if (activeGlyph) {
-                    newGlyphSet.set(activeGlyph, newCanvas)
-                  }
-                  return newGlyphSet
-                })
-              }}
-              mouseDownFlag={mouseDownFlag}
-              setMouseDownFlag={setMouseDownFlag}
-              drawFlag={drawFlag}
-              setDrawFlag={setDrawFlag}
-            />
-          ))}
-        </div>
-      </div>
+      ((x + canvasSize) % canvasSize) + (y + (canvasSize % canvasSize)) * canvasSize
     )
   }
-  return <div />
+
+  const updateCell = (idx: number, filled: boolean) => {
+    if (!glyphCanvas) {
+      return
+    }
+    setGlyphSet(oldGlyphSet => {
+      const newGlyphSet = new Map(oldGlyphSet)
+      const newGlyphCanvas = [...glyphCanvas]
+      newGlyphCanvas[idx] = filled
+      if (activeGlyph) {
+        newGlyphSet.set(activeGlyph, newGlyphCanvas)
+      }
+      return newGlyphSet
+    })
+  }
+
+  const handleMouse = (evt: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (!canvasRef.current || !glyphCanvas) {
+      return
+    }
+
+    const idx = getMousePos(canvasRef.current, evt)
+    if (evt.type === 'mousedown' && evt.buttons === 1) {
+      setMouseDownFlag(true)
+      setDrawFlag(!glyphCanvas[idx])
+      updateCell(idx, !glyphCanvas[idx])
+    } else if (evt.type === 'mouseup' || evt.type === 'mouseleave') {
+      setMouseDownFlag(false)
+    } else if (evt.type === 'mouseover' && evt.buttons === 1) {
+      setMouseDownFlag(true)
+    } else if (evt.type === 'mousemove' && mouseDownFlag) {
+      updateCell(idx, drawFlag)
+    } else {
+      return
+    }
+  }
+
+  return (
+    <div>
+      <div className={styles.header}>
+        <div className={styles.text}>{activeGlyph}</div>
+        <div className={styles.separator} />
+        <Delete
+          className={styles.deleteIcon}
+          onClick={() =>
+            setGlyphSet(oldGlyphSet => {
+              const newGlyphSet = new Map(oldGlyphSet)
+              const newGlyphCanvas = new Array<boolean>(canvasSize ** 2).fill(false)
+              if (activeGlyph) {
+                newGlyphSet.set(activeGlyph, newGlyphCanvas)
+              }
+              return newGlyphSet
+            })
+          }
+        />
+      </div>
+      <canvas
+        ref={canvasRef}
+        className={styles.canvas}
+        width={CANVAS_SIZE}
+        height={CANVAS_SIZE}
+        onMouseDown={evt => handleMouse(evt)}
+        onMouseUp={evt => handleMouse(evt)}
+        onMouseMove={evt => handleMouse(evt)}
+        onMouseOver={evt => handleMouse(evt)}
+        onMouseLeave={evt => handleMouse(evt)}
+      />
+    </div>
+  )
 }
 
 export default Canvas
