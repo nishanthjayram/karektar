ee1dfde02e47e8700ce3c6f4e06aab6f12bd5f2e
Nishanth Jayram <nishanthjayram@gmail.com>
2023-03-10 23:24:13 -0800

    Renamed main canvas size constant; added constant for glyph canvas size.

--- src/constants.ts
+++ src/constants.ts
@@ -7,9 +7,12 @@ export const RX_NON_ALPHANUMERIC = /[^A-Za-z0-9\s]/g
 export const DEFAULT_PROMPT = 'sphinx of black quartz judge my vow'
 
 // Canvas
-export const CANVAS_SIZE = 400
+export const EDITOR_SIZE = 400
 export const EMPTY_CELL = '#efefef'
 export const FILLED_CELL = '#2b2b2b'
+
+// Glyph
+export const GLYPH_SIZE = 48
 
 // GlyphSet
 export const PAGE_LENGTH = 30 // Number of glyphs to display in a page
