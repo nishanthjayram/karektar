62b0383f48eb75ff6dd4ebf4c7bbe68507af0e8e
Nishanth Jayram <nishanthjayram@gmail.com>
2023-03-24 19:27:20 -0700

    Added ChicagoFLF font to project.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
+import './fonts/ChicagoFLF.ttf'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
