a1302ef340e289313cb430fb7660832ddab283e4
Nishanth Jayram <nishanthjayram@gmail.com>
2023-06-05 20:53:59 -0700

    Moved regex definitions back to constants file.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+export const RX_LETTERS = /[A-Za-z]/g
+export const RX_NUMBERS = /[0-9]/g
+export const RX_NON_ALPHANUMERIC = /[^A-Za-z0-9\s]/g
+
 // App
 export const DEFAULT_PROMPT = 'sphinx of black quartz judge my vow'
 export const DEFAULT_SYMBOLS = 'abcdefghijklmnopqrstuvwxyz'.split('')
