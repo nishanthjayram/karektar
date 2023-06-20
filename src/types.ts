ae6fc9df69695b6bfabe0332fc1d05b86687ef07
Nishanth Jayram <nishanthjayram@gmail.com>
2023-06-20 00:59:54 -0700

    Added new types for buttons and menus within the canvas.

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,32 @@
-export type TCanvasTool =
-  | 'DRAW'
-  | 'ERASE'
-  | 'FILL'
-  | 'LINE'
-  | 'RECTANGLE'
-  | 'ELLIPSE'
-export type TCanvasButton = TCanvasTool | 'INVERT' | 'CLEAR' | 'UNDO' | 'REDO'
+import {IconProp} from '@fortawesome/fontawesome-svg-core'
+
+export type TToolLabel = 'DRAW' | 'ERASE' | 'FILL' | 'LINE' | 'RECTANGLE' | 'ELLIPSE'
+export type TActionLabel = 'CLEAR' | 'UNDO' | 'REDO'
+export type TOptionLabel = 'GUIDELINES' | 'MODEL'
+export type TTool = {
+  type: 'tool'
+  label: TToolLabel
+  icon: IconProp
+}
+export type TAction = {
+  type: 'action'
+  label: TActionLabel
+  icon: IconProp
+  disabled: boolean
+}
+export type TOption = {
+  type: 'option'
+  label: TOptionLabel
+  icon: IconProp
+  active: boolean
+}
+export type TButtonType = TTool | TAction | TOption
+
+export type TMenuLabel = 'SHAPES' | 'OPTIONS'
+export type TMenuHeader = {
+  defaultLabel: TMenuLabel
+  defaultIcon: IconProp
+}
 
 export type TPos = [x: number, y: number]
 export type TRect = [x: number, y: number, w: number, h: number]
@@ -13,20 +34,26 @@ export type TShapeRange = [startPos: TPos, endPos: TPos] | undefined
 
 export type TFont = {
   activeGlyph: string
+  activeMenu: TMenuLabel | undefined
   bitmapSize: number
   canvasHistory: boolean[][]
   captureFlag: boolean
-  currentTool: TCanvasTool
+  currentTool: TToolLabel
   glyphSet: Map<string, boolean[]>
+  guidelinesFlag: boolean
   historyIndex: number
+  modelFlag: boolean
   shapeRange: TShapeRange
   symbolSet: string[]
 }
 
 export type TCanvasAction = {type: 'CANVAS_ACTION'} & (
+  | {op: 'UPDATE_ACTIVE_MENU'; newActiveMenu: TMenuLabel | undefined}
   | {op: 'UPDATE_CANVAS_HISTORY'; newGlyphCanvas: boolean[]}
   | {op: 'UPDATE_CAPTURE_FLAG'; newCaptureFlag: boolean}
-  | {op: 'UPDATE_CURRENT_TOOL'; newCurrentTool: TCanvasTool}
+  | {op: 'UPDATE_CURRENT_TOOL'; newCurrentTool: TToolLabel}
+  | {op: 'UPDATE_GUIDELINES_FLAG'; newGuidelinesFlag: boolean}
+  | {op: 'UPDATE_MODEL_FLAG'; newModelFlag: boolean}
   | {op: 'UPDATE_SHAPE_RANGE'; newShapeRange: TShapeRange}
   | {op: 'UNDO'}
   | {op: 'REDO'}
