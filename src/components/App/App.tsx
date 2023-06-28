61d093e372a881156066796861a1d4a24f9678a4
Nishanth Jayram <nishanthjayram@gmail.com>
2023-06-28 12:59:59 -0700

    Changed button behavior; some stylistic changes.
    The erroneously named "Clear" button was renamed to be a "Reset" button,
    with the intention of resetting progress on the current glyph set if the
    user wishes to start over. This is to reflect the change made to the
    reducer action. Additionally, some stylistic changes were made, as well
    as conditions to "disable" certain operations when unnecessary -- e.g.,
    disallowing a reset if no progress has been made to begin with.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,33 @@
-import {useReducer, useState} from 'react'
+import classnames from 'classnames'
+import {useReducer} from 'react'
 import styles from './App.module.scss'
-import {DEFAULT_PROMPT, DEFAULT_SYMBOLS} from '../../utils/constants/app.constants'
-import {getUniqueCharacters, initializeFont} from '../../utils/helpers/app.helpers'
+import {
+  DEFAULT_SYMBOLS,
+  RESET_ALERT,
+  SUBMIT_ALERT,
+  WIKI_LINK,
+} from '../../utils/constants/app.constants'
+import {
+  getUniqueCharacters,
+  initializeFont,
+  isEmptyGlyph,
+} from '../../utils/helpers/app.helpers'
 import {fontReducer} from '../../utils/reducers/fontReducer'
 import Canvas from '../Canvas/Canvas'
 import GlyphSet from '../GlyphSet/GlyphSet'
 
 const App = ({bitmapSize}: {bitmapSize: number}) => {
-  const [inputText, setInputText] = useState(DEFAULT_PROMPT)
   const [fontState, fontDispatch] = useReducer(
     fontReducer,
     initializeFont(bitmapSize, DEFAULT_SYMBOLS),
   )
-
-  const {symbolSet} = fontState
+  const {glyphSet, inputText, symbolSet} = fontState
 
   const handleSubmit = () => {
     const newSymbolSet = getUniqueCharacters(inputText)
     if (
       symbolSet.some(symbol => !newSymbolSet.includes(symbol)) &&
-      !confirm(
-        'You are about to remove some of the existing glyphs in your set. Are you sure you want to continue?',
-      )
+      !confirm(SUBMIT_ALERT)
     ) {
       return
     }
@@ -32,35 +38,60 @@ const App = ({bitmapSize}: {bitmapSize: number}) => {
     })
   }
 
+  const handleReset = () => {
+    if (!confirm(RESET_ALERT)) {
+      return
+    }
+    fontDispatch({
+      type: 'GLYPH_SET_ACTION',
+      op: 'RESET_GLYPH_SET',
+    })
+  }
+
   return (
     <div>
-      <h1>Karektar</h1>
-      <div>
-        <textarea
-          id="queryField"
-          name="queryField"
-          value={inputText}
-          placeholder="Enter prompt"
-          onChange={e => setInputText(e.target.value)}
-          className={styles.input}
-        />
-        <div className={styles.buttonRow}>
-          <button onClick={handleSubmit} className={styles.button}>
-            Submit
-          </button>
-          <button // TODO: Potentially rename this operation to make its meaning clearer.
-            onClick={() =>
-              fontDispatch({
-                type: 'GLYPH_SET_ACTION',
-                op: 'CLEAR_GLYPH_SET',
-              })
-            }
-            className={styles.button}
-          >
-            Clear
-          </button>
-        </div>
+      <h1>
+        <a href={WIKI_LINK} target="_blank" rel="noreferrer noopener">
+          Kare
+        </a>
+        ktar.
+      </h1>
+      <textarea
+        id="queryField"
+        name="queryField"
+        value={inputText}
+        placeholder="Enter prompt"
+        onChange={e =>
+          fontDispatch({
+            type: 'GLYPH_SET_ACTION',
+            op: 'UPDATE_INPUT_TEXT',
+            newInputText: e.target.value,
+          })
+        }
+        className={styles.input}
+      />
+      <div className={styles.buttonRow}>
+        <button
+          className={classnames(
+            inputText.length === 0 && styles.disabledButton,
+            styles.button,
+          )}
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+        <button
+          className={classnames(
+            [...glyphSet.values()].every(glyph => isEmptyGlyph(glyph)) &&
+              styles.disabledButton,
+            styles.button,
+          )}
+          onClick={handleReset}
+        >
+          Reset
+        </button>
       </div>
+
       <br />
       <div className={styles.appRow}>
         <Canvas fontState={fontState} fontDispatch={fontDispatch} />
