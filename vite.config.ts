81686bca219eabbd64a2d1b8e4bb1e6b4efbe9a8
Nishanth Jayram <nishanthjayram@gmail.com>
2023-07-23 22:04:44 -0700

    Changes to config files.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    sourcemap: true,
+  },
   plugins: [svgr(), react()],
 })
