97de4c39b9031a9e5373f3544140d9044cfa76aa
Nishanth Jayram <nishanthjayram@gmail.com>
2025-02-19 01:19:36 -0800

    .

--- functions/auth/[[catchall]].js
+++ functions/auth/[[catchall]].js
@@ -3,16 +3,12 @@ export async function onRequest(context) {
     const { request } = context
     const originalUrl = new URL(request.url)
 
-    // Set the target worker URL
     const workerOrigin = new URL('https://karektar-api.nishanthjayram.workers.dev')
 
-    // Build a new URL using the original pathname and query,
-    // but replace the host and protocol with that of the worker.
     const targetUrl = new URL(request.url)
     targetUrl.protocol = workerOrigin.protocol
     targetUrl.hostname = workerOrigin.hostname
 
-    // Forward the request to the worker.
     return await fetch(targetUrl.toString(), request)
   } catch (error) {
     console.error('Error in auth proxy function:', error)
