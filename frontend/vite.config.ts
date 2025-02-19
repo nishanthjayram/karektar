16956ef50fbdb9ab6a99ed7201c5e2ed6a0d1ac2
Nishanth Jayram <nishanthjayram@gmail.com>
2025-02-18 21:29:44 -0800

    fixed base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
